0df7e5b204f9b7e724555ba2916cba159b13cb07
Максим Гладков <maksimgladkov@Mac-mini-Maksim.local>
2024-12-21 17:57:56 +0700

    Added swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,11 +12,28 @@ export class Routes {
   registerRoutes(app: any) {
     // BLOCKCHAIN
 
+    /**
+     * @swagger
+     * tags:
+     *   - name: ADDRESS
+     *     description: Address related endpoints
+     * /api/mostChangedBalance:
+     *   get:
+     *     tags: [ADDRESS]
+     *     summary: Request to the API to obtain the address whose balance has changed more than others (in absolute value) over the last 100 blocks. Based on the last block number in the network.
+     *     responses:
+     *       200:
+     *        description: The address
+     *       500:
+     *        description: Internal server error
+     */
     app.get(
       '/api/mostChangedBalance',
       async (req: any, res: any) =>
         await this.addressController.mostChangedBalance(req, res)
     )
 
+    // SWAGGER
+    app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
   }
 }
